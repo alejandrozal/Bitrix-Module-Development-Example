9bce926c2ec2ea0c48ec16b6baeefc3f3c995831
zalogin365 <a.zalogin@outlook.com>
2023-06-01 18:43:11 +0300

    migrations fix

---
+++ local/php_interface/migrations/Version20230601183949.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Sprint\Migration;
+
+
+class Version20230601183949 extends Version
+{
+    protected $description   = "Миграция тестовых элементов в hl блок \"Заказы\"";
+    protected $moduleVersion = "4.2.4";
+
+    /**
+     * @throws Exceptions\MigrationException
+     * @throws Exceptions\RestartException
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function up()
+    {
+        $this->getExchangeManager()
+             ->HlblockElementsImport()
+             ->setExchangeResource('hlblock_elements.xml')
+             ->setLimit(20)
+             ->execute(function ($item) {
+                 $this->getHelperManager()
+                      ->Hlblock()
+                      ->addElement(
+                          $item['hlblock_id'],
+                          $item['fields']
+                      );
+             });
+    }
+
+    /**
+     * @throws Exceptions\MigrationException
+     * @throws Exceptions\RestartException
+     * @throws Exceptions\HelperException
+     * @return bool|void
+     */
+    public function down()
+    {
+    }
+
+
+}
